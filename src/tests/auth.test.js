dfab74c154c55ffda287a61f920a6da933ac382c
Yash Kansara <82724814+yash06112002@users.noreply.github.com>
2024-12-09 12:19:17 +0530

    test logs (#1)
    * test logs
    
    * add env vars in github actions
    
    * removed unnecessary logs

--- src/tests/auth.test.js
+++ src/tests/auth.test.js
@@ -10,12 +10,12 @@ describe('Auth Endpoint Tests', () => {
     afterAll(async () => {
         await User.deleteMany({});
     });
-    
+
     it('should register a new user', async () => {
         const res = await request(app)
             .post('/api/auth/register')
             .send({ email: 'test@example.com', password: 'password123', name: 'yash', });
-            
+
         expect(res.statusCode).toEqual(201);
         expect(res.body).toHaveProperty('token');
     });
@@ -24,6 +24,7 @@ describe('Auth Endpoint Tests', () => {
         const res = await request(app)
             .post('/api/auth/login')
             .send({ email: 'test@example.com', password: 'password123' });
+
         expect(res.statusCode).toEqual(200);
         expect(res.body).toHaveProperty('token');
     });
@@ -32,6 +33,7 @@ describe('Auth Endpoint Tests', () => {
         const res = await request(app)
             .post('/api/auth/login')
             .send({ email: 'wrong@example.com', password: 'wrongpassword' });
+
         expect(res.statusCode).toEqual(400);
     });
 });
